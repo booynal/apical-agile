ffbdc189d99903e13edfaa23b8bf04ef40349dfc
张黄江 <xaigb@163.com>
2017-02-05 21:20:26 +0800

    增加帮助命令中描述如何指定文件
    Signed-off-by: 张黄江 <xaigb@163.com>

--- Q9/src/src/main/java/com/apical/ziv/q9/commands/HelpCommand.java
+++ Q9/src/src/main/java/com/apical/ziv/q9/commands/HelpCommand.java
@@ -14,6 +14,7 @@ public class HelpCommand implements Command {
 	@Override
 	public void execute() {
 		System.out.println("This application can handle shape, such as clac area, or if a point is in a shape, and so on.");
+		System.out.println("You can give the file path via -DinputFile=/file/to/path, and then program will read the file as console");
 	}
 
 }
